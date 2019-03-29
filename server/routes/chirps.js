f4dcba7dea4277f6aebacffc172103e12b7db3b2
Heather Till <heather.till@charter.net>
2019-03-29 10:36:13 -0500

    -frontend connected

--- server/routes/chirps.js
+++ server/routes/chirps.js
@@ -1,34 +1,30 @@
 // This file handles all of the api routes for the chirps resource
 
 const express = require('express');
-const chirpsStore = require('../chirpstore')
+const chirpsStore = require('../chirpstore');
 const fs = require('fs');
 let router = express.Router();
 
-
 router.get('/:id?', (req, res) => {
-    let id = req.params.id
-    
-        
+    let id = req.params.id;
+
     if (id) {
-        res.json(chirpsStore.GetChirp(id));  //sets the right content header
+        res.json(chirpsStore.GetChirp(id)); //sets the right content header
     } else {
         res.send(chirpsStore.GetChirps());
     }
 });
 
-
 router.post('/', (req, res) => {
     chirpsStore.CreateChirp(req.body);
     res.send(chirpsStore.GetChirps());
-    res.sendStatus(200)
-})
+    // res.sendStatus(200);
+});
 
 router.put('/:id', (req, res) => {
     let id = req.params.id;
     let chirp = req.body;
 
-   
     if (id) {
         chirpsStore.UpdateChirp(id, chirp);
         res.sendStatus(200);
@@ -36,14 +32,12 @@ router.put('/:id', (req, res) => {
         chirpsStore.CreateChirp(req.body);
         res.sendStatus(200);
     }
-})
+});
 
 router.delete('/:id', (req, res) => {
     let id = req.params.id;
     chirpsStore.DeleteChirp(id);
     res.sendStatus(200);
-})
-
-
+});
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -4,9 +4,10 @@ const apiRouter = require('./routes')
 
 let app = express();
 
+
 app.use(cors());
 app.use(express.json());  //parses json content posted to api. Can use this content like a js object
-
+app.use(express.static('client'));
 app.use('/api', apiRouter);  //register routes -- all routes will be prefixed with /api
 
 

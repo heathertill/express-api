4cd0ba90992068de74fa596e5a65c5bf85de6dbd
Heather Till <heather.till@charter.net>
2019-04-02 16:38:32 -0500

    -

--- client/js/app.js
+++ client/js/app.js
@@ -12,58 +12,52 @@ const displayChirps = data => {
     chirps.reverse();
     chirps.forEach(chirp => {
         $(`#postChirps`).append(`
-        <div class="card m-3 p-3 border">
-            <div class="postBody">
-            <div class="text-right p-1">
-            <button onClick="deleteChirp(${
-                chirp.id
-            })" class="btn btn-sm btn-outline-light text-right py-0 px-1" id="x-button">X</button>
-            </div>
-            <div class="card-body p-2 m-4">
-                <div class="card-title">
-                    <h3>${chirp.user}</h3>
-                </div>
+        <div class="card border m-3 p-3">
+            <div class="color-pea">
+                <div class="p-1 text-right">
+                    <h5>Delete<span>
+                    <button class="btn btn-sm btn-outline-light py-0 px-1 text-right peach" onClick="deleteChirp(${chirp.id})" >X</button>
+                    </span>
+                    </h5>
+                    </div>
+                <div class="p-2 m-4 color-white">
+                    <div class="card-title">
+                        <h3>${chirp.user}</h3>
+                    </div>
                 <div class="card-text ml-3">
                     <h4>${chirp.text}</h4>
                 </div>
             </div>
-            <div class="text-right p-2">
-                <button type="button" class="btn py-0 btn-outline-light" data-toggle="modal" data-target="#chirpModal${
-                    chirp.id
-                }">
-                Edit
+            <div class="p-2 text-right">
+                <button class="btn btn-outline-light py-0" data-toggle="modal" data-target="#chirpModal${chirp.id}">
+                Edit Chirp
                 </button>
             </div>
             </div>
         </div>
 
-        <div class="modal" id="chirpModal${
-            chirp.id
-        }" tabindex="-1" role="dialog">
+        <div class="modal" id="chirpModal${chirp.id}" tabindex="-1" role="dialog">
             <div class="modal-dialog" role="document">
-            <div class="modal-div p-3">
-                <div class="modal-content p-2">
-                    <div class="m-1 pl-2 pb-2">
-                        
-                        <button type="button" class="close py-0 px-1" data-dismiss="modal" id="x-button" aria-label="Close">
-                        <span aria-hidden="true">&times;</span> 
-                        </button>
+                <div class="p-3 color-white">
+                    <div class="modal-content p-2 color-pea">
+                        <div class="m-1 pl-2 pb-2">
+                            <button class="close py-0 px-1" data-dismiss="modal" id="x-button" aria-label="Close">
+                                <span aria-hidden="true">&times;</span> 
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <input class="input-border p-2" type="text" size="75" value="${chirp.user}" id="editChirpUser${chirp.id}">
+                            <br>
+                            <input class="input-border p-2" type="text" size="75" value="${chirp.text}" id="editChirpText${chirp.id}">
+                        </div>
+                        <div class="text-right pt-3 pl-3">
+                            <button type="button" class="btn btn-outline-light" data-dismiss="modal">Close</button>
+                            <button onClick="editChirp(${chirp.id})" type="button" class="btn btn-outline-light" data-dismiss="modal">Save changes</button>
+                        </div>
                     </div>
-                <div class="modal-body">
-                    <input type="text" size="75" value="${chirp.user}" id="editChirpUser${chirp.id}">
-                    <br>
-                    <input type="text" size="75" value="${chirp.text}" id="editChirpText${chirp.id}">
-                </div>
-                <div class="text-right pt-3 pl-3">
-                    <button type="button" class="btn btn-outline-light" data-dismiss="modal">Close</button>
-                    <button onClick="editChirp(${
-                        chirp.id
-                    })" type="button" class="btn btn-outline-light" data-dismiss="modal">Save changes</button>
                 </div>
             </div>
         </div>
-        </div>
-</div>
         `);
     });
 };
